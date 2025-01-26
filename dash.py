0e222e5448b396107bb3d20346472de7c297c7f0
juniorjailton <65622365+juniorjailton@users.noreply.github.com>
2025-01-26 12:10:15 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSnNn7e3b6WSGzOagWF77LyjNSRg6PJgHZ0No056Mp6w26A-g1AzjBWqJlvYZIPJkDmi-Fq2KDxDhzs/pub?gid=733224137&single=true&output=csv')
-
+df = df.dropna()
 
 #começo
 st.write(""" # Registro de Estudos """)
